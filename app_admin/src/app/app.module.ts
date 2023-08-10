c1c49a1a62db7d8ce425a807508341ddbd600c8b
Tyler Hollingsworth <tholli7349@gmail.com>
2023-08-10 12:14:50 -0400

    modified: src/app/app.module.ts to include an import for FormsModule

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { AppRoutinModule } from './app-router.module';
@@ -28,6 +28,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     ReactiveFormsModule,
     AppRoutinModule
   ],
